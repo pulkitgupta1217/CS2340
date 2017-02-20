70f937aaa8cf6998b5644a680a2ab203a93c2999
Pulkit Gupta <Pulkit Gupta>
2017-02-20 18:24:35 -0500

    adding pseudo code to list of necessary classes

---
+++ C++urry/ClassesWeNeed.java
@@ -0,0 +1,144 @@
+class User {
+    private String user, pass, email;
+    private UserType type;
+    private boolean banned = false;
+    private int id;
+
+    public User(String user, String pass, String email) {
+        this.user = user;
+        this.pass = pass;
+        if (email == "" || email == null) {
+            email = user + "@emails.com";
+        }
+        this.email = email;
+        type = UserType.USER;
+        setID();
+    }
+    public String getUser() {
+        return user;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public boolean isBanned() {
+        return banned;
+    }
+    public void setType(UserType newType)  {
+        type = newType;
+    }
+    public void setBan(boolean status) {
+        banned = status;
+    }
+    public void generateReport() {
+        if (banned) {
+            throw new BannedUserException(user + " is banned from generating reports");
+        }
+        Report report = new Report(user);
+        report.setText(/* some method to make a report*/);
+        firebase.add(report);
+    }
+    public void viewReports(){
+        List<Report> reports = firebase.getAsList("reports");
+        View reportView = new View(/* code to show the list*/);
+    }
+    public void editReport(Report report) {
+        report.editText(/*code to edit report*/);
+        firebase.edit(report);
+    }
+
+    public void setID() {
+        id = user.hashCode() *  100;
+        id += (pass.hashCode() * 100);
+        id += (email.hashCode() * 100);
+        id += (type.value().toString().hashCode());
+    }
+    public int getID() {
+        return id;
+    }
+
+}
+
+class Worker extends User {
+    public Worker(String user, String pass, String email) {
+        super(user, pass, email);
+        setType(WORKER);
+        setID();
+    }
+    public void generatePurityReport() {
+        Report report = new PurityReport(user);
+        report.setText(/* some method to make a report*/);
+        firebase.add(report);
+    }
+}
+
+class Manager extends Worker {
+    public Manager(String user, String pass, String email) {
+        super(user, pass, email);
+        setType(MANAGER);
+        setID();
+    }
+    public void removeReport(Report report) {
+        firebase.removeByID(report.getID());
+    }
+    public void viewHistoricalReports() {
+        List<Report> historicalReports = firebase.getAsList("historical_reports");
+        View reportView = new View(/* code to show the list*/);
+    }
+    public void viewPurityTrends() {
+        List<Report> purityTrends = firebase.getAsList("purity_trends");
+        View trendView = new View(/* code to show the list*/);
+    }
+}
+
+class Admin extends Manager {
+    public Worker(String user, String pass, String email) {
+        super(user, pass, email);
+        setType(ADMIN);
+        setID();
+    }
+    public void deleteAccount(User account) {
+        firebase.removeByID(account.getID());
+    }
+    public void banUser(User user) {
+        user.setBan(true);
+    }
+    public void unBanUser(User user) {
+        user.setBan(false);
+    }
+    public void viewSecurityLog() {
+        File log = new File(firebase.getSecurityLog());
+        View logView = new View(/* code to show log*/);
+    }
+
+}
+
+
+
+
+class Report {
+    public File backingFile;
+    public String user, info;
+
+    public Report(String user) {
+        this.user = user;
+    }
+    public void setMessage(String newMessage) {
+        info = newMessage;
+    }
+    public String getMessage() {
+        return info;
+    }
+    public void generateReport() {
+        backingFile.println(user);
+        backingFile.println(info);
+    }
+}
+
+class PurityReport extends Report {
+    public PurityReport(String user) {
+        super(user);
+    }
+}
