3c88d319a95622253fde479dcdaddbab31b78e79
Danny Zhang <zhangdanny96@gmail.com>
2017-04-26 19:04:20 -0400

    added view water reports

---
+++ C++urry/WebApp/assets/js/services/viewwaterreports.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var rootRef = firebase.database().ref().child("reports");
+    rootRef.on("child_added", snap => {
+        var key = snap.getKey();
+        var creator = snap.child("creator").val();
+        var dateTime = snap.child("dateTime").val();
+        var lat = snap.child("lat").val();
+        var lng = snap.child("lng").val();
+        var waterCondition = snap.child("waterCondition").val();
+        var waterType = snap.child("waterType").val();
+        $("#waterList").append("<tr><td>" + key + "</td><td>" + creator + "</td><td>" + dateTime + "</td><td>" + lat 
+            + "</td><td>" + lng + "</td><td>" + waterCondition + "</td><td>" + waterType + "</td></tr>");
+    });
+};
