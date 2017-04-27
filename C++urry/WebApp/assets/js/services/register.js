8fa74ec4b3399c124c990515d108ba0628ebe07e
Danny Zhang <zhangdanny96@gmail.com>
2017-04-26 23:40:46 -0400

    reports

--- C++urry/WebApp/assets/js/services/register.js
+++ C++urry/WebApp/assets/js/services/register.js
@@ -18,13 +18,67 @@ function registerClick() {
         if (errorCode == 'auth/email-already-in-use') {
                         alert('Email already in use.');
                 } else {
-                        alert('Successfully registered.');
+                        alert(errorMessage);
             }
     }).then(function() {
-        
-        window.location.href = "login.html";
+        firebase.auth().signInWithEmailAndPassword(user , pass).catch(function(error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // [START_EXCLUDE]
+                  if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                  } else {
+                    alert(errorMessage);
+                  }
+                  console.log(error);
+                  document.getElementById('quickstart-sign-in').disabled = false;
+                  // [END_EXCLUDE]
+                }).then(function() {
+                    var user1 = firebase.auth().currentUser;
+                    valid = user1 != null;
+                    if (valid) {
+                      addCookie(user1, user, pass);
+                    } else {
+                      // failure
+                    }
+                    var key = Cookies.getJSON('user').uid
+                    var data = {
+                        "username": user,
+                        "email": Cookies.getJSON('user').email,
+                        "banned": false,
+                        "address": "NO ADDRESS",
+                        "phone": "PHONE",
+                        "username": Cookies.getJSON('user').email,
+                        "name": "newName"
+                    };
+                    alert(data);
+                    firebase.database().ref('users/' + key).set(data).then(
+                        function() {
+                            lit();
+                        });
+                    
+                });
     });
-    return false;
+    
+
+        
+
+                    
 
 }
+
+function addCookie(obj, email, pword) {
+    Cookies.set('user', obj, {path: "/", expires: 30});
+    Cookies.set('email', email);
+    Cookies.set('password', pword);
+    alert("set");
+}
+
+function lit() {
+
+        window.location.href = "viewwaterreports.html";
+}
+
+
 
