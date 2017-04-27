c7b03dccdabb2f716306cdc6f1ff2306db5c1f15
Machado, Brendon <brendonmachado@lawn-143-215-127-51.lawn.gatech.edu>
2017-04-26 22:04:32 -0400

    updated tab views

--- C++urry/WebApp/assets/js/services/register.js
+++ C++urry/WebApp/assets/js/services/register.js
@@ -21,6 +21,7 @@ function registerClick() {
                         alert('Successfully registered.');
             }
     }).then(function() {
+        
         window.location.href = "login.html";
     });
     return false;
---
+++ C++urry/WebApp/assets/js/services/tabs.js
@@ -0,0 +1,45 @@
+window.onload = manageTabs();
+
+function manageTabs() {
+	checkSignedIn();
+	// var x = Cookies.get('user');
+	// alert(typeof(x));
+	// var str = x.uid;
+	// alert(str);
+	var ref = firebase.database().ref('users/' + Cookies.getJSON('user').uid);
+	var uType;
+	ref.once("value").then(function(snapshot) {
+    	uType = snapshot.child("userType").val(); // "last"
+		if (uType == null) {
+			uType = "USER";
+		}
+		hide(uType);
+  	});
+}
+
+function hide(uType) {
+	//user: view water report
+
+
+	//admin: view everything
+	//manager: not view accounts
+	//
+	if (uType == "USER") {
+		hideTab("viewGraph");
+		hideTab("viewAccounts");
+		hideTab("createPurityReport");
+		hideTab("viewPurityReports");
+		hideTab("viewSecurityLogs");
+	} else if (uType == "WORKER") {
+		hideTab("viewAccounts");
+		hideTab("viewGraph");
+		hideTab("viewSecurityLogs");
+	} else if (uType == "MANAGER") {
+		hideTab("viewAccounts");
+		hideTab("viewSecurityLogs");
+	}
+}
+
+function hideTab(tabID) {
+	document.getElementById(tabID).setAttribute("style", "display: none;");
+}
